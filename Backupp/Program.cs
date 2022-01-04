5737b5c1a138e7f3fe1fed119d30236047d8a6b3
anrikekw <anri.aleksandria@gmail.com>
2022-01-04 22:39:21 +0400

    backup

---
+++ Backupp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+
+namespace Backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string DirToCheck = args[0];
+            string DesDir = args[1];
+            string time = $"{args[2]} {args[3]}";
+
+            DateTime Pasttime;
+            if (!DateTime.TryParse(time, out Pasttime))
+            {
+                Console.WriteLine("wrong input");
+                return;
+            }
+
+
+            Backup(DirToCheck, Pasttime, DesDir);
+        }
+
+        static void Backup(string path, DateTime PastTime, string destination)
+        {
+            if (Directory.Exists(path) && Directory.Exists(destination))
+            {
+                var files = Directory.GetFiles(path);
+
+                foreach (var file in files)
+                {
+                    var CreationDate = File.GetCreationTime(file);
+                    if (CreationDate.CompareTo(PastTime) > 0)
+                    {
+                        string name = Path.GetFileName(file);
+                        string filedest = Path.Combine(destination, name);
+                        File.Copy(file, filedest, true);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("wrong folders");
+            }
+
+        }
+    }
+}
